f0591035a98624bbca0ce48c4fcabb4c85d736b9
mudia <st168854@stud.uni-stuttgart.de>
2023-07-25 02:01:14 +0200

    update all

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
-// import {dat} from './app.js'
 import getData from './app.js'
+
+
 
 
 getData().then((data) => {
     const retrievedData = data
-    console.log(retrievedData)
+    // console.log(retrievedData)
     // NEXT: USE DATA IN THE DIFFERENT HTML
     // But First Lets Test It
-    const selectCrew = document.querySelector('.select-crew')
+    // CREW
+    try{const selectCrew = document.querySelector('.select-crew');
     selectCrew.addEventListener('click', (e)=>{
-        console.log(((Number(e.target.className))))
-        try {changeCrew(Number(e.target.className), retrievedData)}
-        catch{console.log('click a button')}
-        
-    })
-    const selectDest = document.querySelector('.select-destination')
+        try {
+            resetBg(selectCrew)
+            changeCrew(Number(e.target.className), retrievedData)
+            e.target.style.backgroundColor = '#ffffff'
+            }
+        catch{}
+    })}    
+    catch{}
+
+    // DESTINATION
+    try {const selectDest = document.querySelector('.select-destination')
     selectDest.addEventListener('click', (e)=>{
-        console.log(((Number(e.target.className))))
-        try {changeDestination(Number(e.target.className), retrievedData)}
-        catch{console.log('click a button')}
-        
-    })
-    const selectTech = document.querySelector('.select-technology')
-    selectTech.addEventListener('click', (e)=>{
-        console.log(((Number(e.target.className))))
-        try {changeTechnology(Number(e.target.className), retrievedData)}
-        catch{console.log('click a button')}
-        
-    })
+        try {
+            resetDestBg(selectDest)
+            changeDestination(Number(e.target.className), retrievedData)
+            e.target.style.borderBottom = '2px solid #ffffff'
+            e.target.style.color = '#ffffff'
+        }
+        catch{}   
+    })}
+    catch{}
+    
+    // TECHNOLOGY
+    try{const selectTech = document.querySelector('.select-technology')
+        selectTech.addEventListener('click', (e)=>{
+            // console.log(((Number(e.target.className))))
+            try {
+                resetTechBg(selectTech)
+                changeTechnology(Number(e.target.className), retrievedData)
+                e.target.style.backgroundColor = '#ffffff'
+                e.target.style.color = '#0B0D17'
+            }
+            catch{//console.log('click a button')
+            }     
+    })}
+    catch{// console.log('not in technology')
+    }
 })
 
+// Updating functions
 function changeCrew(num, retData) {
     const x = retData.crew[num]
     document.getElementById('crew-name').textContent=x.name
@@ -38,22 +59,48 @@ function changeCrew(num, retData) {
     document.getElementById('crew-bio').textContent=x.bio
 }
 function changeDestination(num, retData) {
-    const x = retData.crew[num]
+    const x = retData.destinations[num]
     document.getElementById('destination-name').textContent=x.name
     document.getElementById('destination-photo').src=x.images.png
-    document.getElementById('description').textContent=x.description
+    document.getElementById('destination-description').textContent=x.description
     document.getElementById('distance').textContent=x.distance
     document.getElementById('travel').textContent=x.travel
 }
 function changeTechnology(num, retData) {
-    const x = retData.crew[num]
+    const x = retData.technology[num]
     document.getElementById('technology-name').textContent=x.name
-    document.getElementById('technology-photo').src=x.images.png
-    document.getElementById('technology-role').textContent=x.role
-    document.getElementById('technology-bio').textContent=x.bio
+    document.getElementById('technology-photo').src=x.images.landscape
+    document.getElementById('technology-description').textContent=x.description
 }
 
-// const 
+// RESET PAGE INDICATION FUNCTIONS
+function resetBg (selectList){
+    const nodes = selectList.childNodes
+    nodes.forEach((e) => {
+        try{e.style.backgroundColor = '#97979795'}
+        catch{'what happened'}
+    });
+}
+// tech text color 0B0D17...fill white,, border none on click
+// else text color whit...fill transparent, border '#97979795'
+function resetTechBg (selectList){
+    const nodes = selectList.childNodes
+    nodes.forEach((e) => {
+        try{
+            e.style.backgroundColor = 'transparent'
+            e.style.color = '#ffffff'
+        }
+        catch{'what happened'}
+    });
+}
 
-// write a function that maps various values to the dom
-// write a function that
+function resetDestBg (selectList){
+    const nodes = selectList.childNodes
+    nodes.forEach((e) => {
+        try{
+            e.style.borderBottom = 'transparent'
+            e.style.color = '#D0D6F9'
+        }
+        catch{'what happened'}
+    });
+}
